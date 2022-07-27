07992afb2aafe724afe247142a217b9dae56f5f1
zyjava <zyjava@qq.com>
2022-07-27 22:47:46 +0800

    使用spring cache 简化redis操作

--- src/main/java/com/zhuoyue/reggie/controller/SetmealController.java
+++ src/main/java/com/zhuoyue/reggie/controller/SetmealController.java
@@ -13,6 +13,8 @@ import com.zhuoyue.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,6 +32,7 @@ public class SetmealController {
     private CategoryService categoryService;
 
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
           log.info("套餐信息：{}",setmealDto);
           setmealService.saveWithDish(setmealDto);
@@ -70,6 +73,7 @@ public class SetmealController {
     }
 
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(Long[] ids){
 
         setmealService.deleteWithDish(ids);
@@ -88,6 +92,7 @@ public class SetmealController {
     }
 
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId+'_'+#setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> lqw = new LambdaQueryWrapper<>();
         lqw.eq(setmeal.getCategoryId() != null,Setmeal::getCategoryId,setmeal.getCategoryId());
